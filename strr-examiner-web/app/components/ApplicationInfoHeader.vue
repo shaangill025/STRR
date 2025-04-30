077574001f2c20e21769258b99e3b851c911902a
Shaanjot Gill <gill.shaanjots@gmail.com>
2025-04-30 11:34:17 -0700

    27281 - design feedback
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-examiner-web/app/components/ApplicationInfoHeader.vue
+++ strr-examiner-web/app/components/ApplicationInfoHeader.vue
@@ -66,13 +66,6 @@ const registrationCountdown = computed(() => {
             {{ activeHeader?.applicationNumber }}
           </span>
           <span
-            v-if="activeHeader?.registrationId"
-            class="cursor-pointer border-r-2 border-gray-700 pr-3 font-bold text-bcGovColor-activeBlue underline"
-            @click="goToRegistration(activeHeader?.registrationId)"
-          >
-            {{ activeHeader?.registrationNumber }}
-          </span>
-          <span
             v-if="getApplicationName()"
             class="border-r-2 border-gray-700 pr-3"
           >
