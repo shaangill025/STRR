66a4162b94d3d6ff27cc06ee49d8c1ec6db5e0e0
Shaanjot Gill <gill.shaanjots@gmail.com>
2025-05-05 08:33:44 -0700

    27281 - ux feedback
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-examiner-web/app/pages/dashboard.vue
+++ strr-examiner-web/app/pages/dashboard.vue
@@ -574,16 +574,20 @@ function handleColumnSort (column: string) {
 
         <!-- row slots -->
         <template #registrationNumber-data="{ row }">
-          <div>
+          <div class="text-sm">
             {{ row.applicationNumber }}
           </div>
-          <div
+          <UButton
             v-if="row.registrationNumber"
-            class="flex cursor-pointer items-center whitespace-nowrap font-bold text-bcGovColor-activeBlue underline"
+            icon="i-mdi-check-circle"
+            variant="link"
+            :padded="false"
+            :ui="{ gap: { sm: 'gap-x-1' }, icon: { base: 'mr-1 text-green-700 text-xs' } }"
+            class="text-sm"
             @click="goToRegistration(row.registrationId)"
           >
-            <UIcon name="i-mdi-check-circle" class="mr-1 text-green-700" />{{ row.registrationNumber }}
-          </div>
+            {{ row.registrationNumber }}
+          </UButton>
         </template>
 
         <template #propertyAddress-data="{ row }">
